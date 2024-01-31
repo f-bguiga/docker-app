03cfad0b74dd90772ceaed7211c42af1e4099a5a
firas bguiga <firasbguiga@gmail.com>
2024-01-31 11:13:24 +0100

    testing docker

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code>this is another update
+          Edit <code>hello</code>testing docker
         </p>
         <a
           className="App-link"
